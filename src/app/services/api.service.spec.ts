e3ba8c7e6663b9d6214df78ae57e415d4c1dea53
walter <walterkiprono81@gmail.com>
2021-07-30 11:04:34 +0300

    added tests

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,45 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
   let service: ApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
   });
 
-  it('should be created', () => {
+  it('should create service: "ApiService"', () => {
+    service = TestBed.inject(ApiService);
     expect(service).toBeTruthy();
+  });
+
+  it('getPatients() should return an Observable value', () => {
+    (done: DoneFn) => {
+      service.getPatients("test").subscribe(value => {
+        expect(value).toBe([]);
+        done();
+      });
+    }
+  });
+
+  it('getPatientsByStatus() should return an Observable value', () => {
+    (done: DoneFn) => {
+      service.getPatientsByStatus("", "").subscribe(value => {
+        expect(value).toBe([]);
+        done();
+      });
+    }
+  });
+
+  it('getReportByMonth() should return an Observable value', () => {
+    (done: DoneFn) => {
+      service.getReportByMonth("").subscribe(value => {
+        expect(value).toBe([]);
+        done();
+      });
+    }
   });
 });
